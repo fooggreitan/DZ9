255af704f41c67ebc7f28ad788c10a95e23cb1fe
LEN4 <fooggreitan@gmail.com>
2022-11-19 23:33:37 +0300

    new1

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,51 +1,41 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 1, 7 -> такого элемента в массиве нет
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
-int[] meaningNumbersArray = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+Console.Write("Введите целовен число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целовен число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
 
-int[,] CreateArrayRndInt(int row, int columns, int min, int max)
+int SumDigitsForward(int numOne, int numTwo)
 {
-    var array = new int[row, columns];
-    var rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
+    if (numTwo == numOne)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = rnd.Next(min, max + 1);
-        }
+        return numOne = numOne + SumDigitsForward(numOne + 1, numTwo);
     }
-    return array;
-}
 
-bool CheckingPositions(int[,] array)
-{
-    if (meaningNumbersArray[0] < array.GetLength(0) && meaningNumbersArray[1] < array.GetLength(1)) return true;
-    else return false;
-}
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
+    if (numOne > numTwo || numTwo == 0) return 0;
     {
-        Console.Write("[");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j], 4}, "); // форматирование
-            else Console.Write($"{array[i, j], 4}"); // форматирование
-        }
-        Console.WriteLine("]");
+        return numOne + SumDigitsForward(numOne + 1, numTwo);
     }
 }
 
-int[,] array2D = CreateArrayRndInt(3, 4, 1, 100);
-PrintArray(array2D);
+int SumDigitsBackward(int numOne, int numTwo)
+{
+    if (numTwo > numOne || numTwo == 0) return 0;
+    {
+        return numOne = numOne + SumDigitsBackward(numOne - 1, numTwo);
+    };
+}
 
-bool сheckingPositions = CheckingPositions(array2D);
-
-if (сheckingPositions) Console.WriteLine($"{array2D[meaningNumbersArray[0], meaningNumbersArray[1]]}");
-else Console.WriteLine("Такого элемента в массиве нет");
+if (numberOne > numberTwo)
+{
+    int sumDigitsBackward = SumDigitsBackward(numberOne, numberTwo);
+    Console.WriteLine($"Cумму натуральных элементов в промежутке от {numberOne} до {numberTwo} = {sumDigitsBackward}");
+}
+else
+{
+    int sumDigitsForward = SumDigitsForward(numberOne, numberTwo);
+    Console.WriteLine($"Cумму натуральных элементов в промежутке от {numberOne} до {numberTwo} = {sumDigitsForward}");
+}
