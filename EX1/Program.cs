1898b09a008f17badde91429fa3cb495cf21f462
LEN4 <fooggreitan@gmail.com>
2022-11-19 23:33:28 +0300

    new1

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,36 +1,18 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-double[,] CreateArrayRndInt(int row, int columns, double min, double max)
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
 {
-    var array = new double[row, columns];
-    var rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = Math.Round(rnd.NextDouble(), 2);
-        }
-    }
-    return array;
-}
+    if (num == 0) return;
+    if(num == 1) Console.Write($"{num}"); 
+    else Console.Write($"{num}, ");     
+    NaturalNumbers(num - 1);
 
-void PrintArray(double[,] array)
-{  
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("[");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (j < array.GetLength(1) - 1) Console.Write($"{array[i,j], 6}, "); // форматирование
-            else Console.Write($"{array[i,j], 6}"); // форматирование
-        }
-        Console.WriteLine("]");
-    }
 }
-
-double[,] array2D = CreateArrayRndInt(3, 4, 1, 100);
-PrintArray(array2D);
+Console.Write($"все натуральные числа в промежутке от {number} до 1 - > ");
+NaturalNumbers(number);
